8b2ce53dedf98a6089ee32220c670562152fae70
unknown <wkwk2805@gmail.com>
2020-04-08 22:43:45 +0900

    게시글 입력부분 화면 완성

--- view/WriteView.js
+++ view/WriteView.js
@@ -2,44 +2,113 @@ import React from "react";
 import {
   View,
   Text,
-  TextInput,
   Image,
   SafeAreaView,
   Dimensions,
+  TextInput,
 } from "react-native";
 import { useSelector } from "react-redux";
 import Constants from "expo-constants";
-import { Asset } from "expo-asset";
+import { IconButton, RadioButton, Checkbox } from "react-native-paper";
+
+const setScope = ["모두", "친구만", "그룹만", "나만", "친구+그룹만"];
+
 const WriteView = () => {
   const { width } = Dimensions.get("window");
   const selectedAssetList = useSelector((s) => s.selectedAssetList);
   return (
     <SafeAreaView style={{ marginTop: Constants.statusBarHeight }}>
+      <View
+        style={{
+          borderBottomWidth: 1,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <IconButton
+          icon="arrow-left"
+          size={25}
+          onPress={() => console.log("Pressed")}
+        />
+        <Text>게시글 입력</Text>
+        <View style={{ position: "absolute", right: 0 }}>
+          <IconButton
+            icon="check"
+            size={25}
+            onPress={() => console.log("Pressed")}
+          />
+        </View>
+      </View>
+      <View style={{ margin: 10, flexDirection: "row" }}>
+        <View style={{ width: 100, height: 100 }}>
+          <Image
+            source={{
+              uri: `https://d33v4339jhl8k0.cloudfront.net/docs/assets/5c814e0d2c7d3a0cb9325d1f/images/5c8bc20d2c7d3a154460eb97/file-1CjQ85QAme.jpg`,
+            }}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </View>
+        <View style={{ maxHeight: 300, width: width - 130, marginLeft: 10 }}>
+          <TextInput
+            multiline
+            numberOfLines={4}
+            maxLength={40}
+            style={{
+              borderWidth: 1,
+              minHeight: 100,
+              width: "100%",
+            }}
+            placeholder="게시글을 입력해주세요..."
+          />
+        </View>
+      </View>
       <View style={{ margin: 10 }}>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ width: 100, height: 100 }}>
-            <Image
-              source={{
-                uri: `https://d33v4339jhl8k0.cloudfront.net/docs/assets/5c814e0d2c7d3a0cb9325d1f/images/5c8bc20d2c7d3a154460eb97/file-1CjQ85QAme.jpg`,
-              }}
-              style={{ width: "100%", height: "100%" }}
-            />
+        <Text>공개범위설정</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          {setScope.map((e) => (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <RadioButton value="first" status="checked" />
+              <Text>{e}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Text>그룹선택</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox onPress={() => console.log("Press")} />
+            <Text>그룹1</Text>
           </View>
-          <View style={{ height: 300, width: width - 120 }}>
-            <TextInput
-              multiline
-              numberOfLines={4}
-              editable
-              maxLength={40}
-              style={{
-                borderWidth: 1,
-                minHeight: 100,
-                width: "100%",
-              }}
-            />
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox onPress={() => console.log("Press")} />
+            <Text>그룹1</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox onPress={() => console.log("Press")} />
+            <Text>그룹1</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox onPress={() => console.log("Press")} />
+            <Text>그룹1</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox onPress={() => console.log("Press")} status="checked" />
+            <Text>그룹1</Text>
           </View>
         </View>
-        <View></View>
       </View>
     </SafeAreaView>
   );
