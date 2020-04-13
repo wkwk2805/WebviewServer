956d71096ab786cd84d570e7de1a999e9415a3fd
unknown <wkwk2805@gmail.com>
2020-04-13 18:03:46 +0900

    다시설정해주기

--- view/Web.js
+++ view/Web.js
@@ -1,6 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Constants from "expo-constants";
-import { KeyboardAvoidingView, AsyncStorage, View, Alert } from "react-native";
+import {
+  KeyboardAvoidingView,
+  AsyncStorage,
+  Alert,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import { WebView } from "react-native-webview";
 import { useDispatch, useSelector } from "react-redux";
 import { removeAllSelectedAsset } from "../modules/selectedAssetList";
@@ -8,10 +15,39 @@ import { showLoading, hideLoading } from "../modules/loading";
 import Loading from "../components/GalleryComp/Loading";
 import { setAlbumInfo } from "../modules/album";
 import { ProgressBar, Colors } from "react-native-paper";
+import { host } from "../host";
 
 const Web = ({ navigation }) => {
+  const webviewRef = useRef();
+  const wait = (timeout) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, timeout);
+      webviewRef.current.reload();
+    });
+  };
+
   const [visible, setVisible] = useState(false);
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, [refreshing]);
+
   const dispatch = useDispatch();
+  const onMessageData = async ({ nativeEvent }) => {
+    const data = JSON.parse(nativeEvent.data);
+    switch (data.branch) {
+      case "Add":
+        await click(data.token);
+        break;
+      case "Refresh":
+        break;
+
+      default:
+        break;
+    }
+  };
   const click = async (token) => {
     if (progress < 1 && progress > 0) {
       Alert.alert("파일을 올리는 중입니다.");
@@ -34,33 +70,53 @@ const Web = ({ navigation }) => {
   return (
     <>
       <Loading />
-      <KeyboardAvoidingView
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-        behavior="height"
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       >
-        <WebView
-          source={{ uri: "http://192.168.25.7:3000" }}
-          onMessage={(event) => click(event.nativeEvent.data)}
-          style={{ marginTop: Constants.statusBarHeight }}
-          renderLoading={() => {
-            dispatch(showLoading());
+        <KeyboardAvoidingView
+          style={{
+            width: "100%",
+            height: "100%",
           }}
-          onLoad={() => dispatch(hideLoading())}
-        />
-
-        {visible && (
-          <ProgressBar
-            progress={progress}
-            color={Colors.blue700}
-            style={{ height: 10 }}
+          behavior="height"
+        >
+          <WebView
+            source={{ uri: `${host()}:3000` }}
+            onMessage={onMessageData}
+            style={{ marginTop: Constants.statusBarHeight }}
+            renderLoading={() => {
+              dispatch(showLoading());
+            }}
+            onLoad={() => dispatch(hideLoading())}
+            ref={webviewRef}
           />
-        )}
-      </KeyboardAvoidingView>
+          {visible && (
+            <ProgressBar
+              progress={progress}
+              color={Colors.blue700}
+              style={{ height: 10 }}
+            />
+          )}
+        </KeyboardAvoidingView>
+      </ScrollView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default Web;
